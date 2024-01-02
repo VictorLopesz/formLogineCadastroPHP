845e0114c1bdf7a0894f8096e10f563e15a7b885
Victor.Loperz <euvictor_h@hotmail.com>
2024-01-02 14:59:26 -0300

    adiciona opção de mostrar/ocultar senha

--- login/index.php
+++ login/index.php
@@ -80,7 +80,7 @@ include("../config/conexao.php");
                         </div>
                     <?php } ?>
 
-                    
+
 
                     <?php if (isset($_GET["sucess"]) && !empty($_GET["sucess"])) { ?>
                         <div id="mensagem_cadastrar">
@@ -101,7 +101,9 @@ include("../config/conexao.php");
                         <div class="input-group-prepend">
                             <span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-key"></i></span>
                         </div>
-                        <input type="password" name="password" class="form-control" placeholder="digite sua senha" aria-label="Password" aria-describedby="basic-addon1">
+                        <input type="password" name="password" id="password" class="form-control" placeholder="digite sua senha" aria-label="Password" aria-describedby="basic-addon1">
+                        <button type="button" id="eyes" name="eyes" onclick="mostrarSenha()"><i class="fa-solid fa-eye"></i></button>
+
                     </div>
 
 
@@ -131,6 +133,17 @@ include("../config/conexao.php");
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <script>
+        function mostrarSenha() {
+            var tipo = document.getElementById("password");
+            if (tipo.type == "password") {
+                tipo.type = "text";
+            } else(
+                tipo.type = "password"
+            )
+        }
+    </script>
 </body>
 
 </html>

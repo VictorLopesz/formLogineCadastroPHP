9314fefd7296fc4e4cc7868d0b9883c8c7d319b4
VictorLopesz <131501443+VictorLopesz@users.noreply.github.com>
2024-01-03 21:10:52 -0300

    Atualizar o conexao.php

--- config/conexao.php
+++ config/conexao.php
@@ -1,12 +1,12 @@
-<?php 
+// <?php 
 
-$hostname = 'localhost';
-$user = 'root';
-$password = '';
-$db = 'usuarios';
+// $hostname = 'localhost';
+// $user = 'root';
+// $password = '';
+// $db = 'usuarios';
 
 // $conn = new mysqli($hostname, $user, $password, $db); 
 
-if ($conn -> connect_errno){
-    echo "Falha ao conectar: (" .$conn->connect_errno.")" . $conn -> connect_error;
-}
+// if ($conn -> connect_errno){
+//    echo "Falha ao conectar: (" .$conn->connect_errno.")" . $conn -> connect_error;
+// }
